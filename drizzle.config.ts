6662ebb870e0641361e4475ce16a891a3225ec75
Obi Fortune <gabrielobi.of@gmail.com>
2024-04-22 21:03:00 -0400

    trpc commit

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'drizzle-kit';
+
+import { env } from '~/env';
+
+export default defineConfig({
+  schema: './src/server/db/schema.ts',
+  out: './drizzle/migrations',
+  driver: 'turso',
+  dbCredentials: {
+    url: env.DATABASE_URL,
+    authToken: env.DATABASE_AUTH_TOKEN,
+  },
+});
---
+++ src/env.js
@@ -0,0 +1,34 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url(),
+    DATABASE_AUTH_TOKEN: z.string(),
+    NODE_ENV: z
+      .enum(['development', 'test', 'production'])
+      .default('development'),
+    NEXTAUTH_SECRET:
+      process.env.NODE_ENV === 'production'
+        ? z.string()
+        : z.string().optional(),
+    NEXTAUTH_URL: z.preprocess(
+      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+      // Since NextAuth.js automatically uses the VERCEL_URL if present.
+      str => process.env.VERCEL_URL ?? str,
+      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+      process.env.VERCEL ? z.string() : z.string().url(),
+    ),
+
+    DISCORD_CLIENT_ID: z.string(),
+    DISCORD_CLIENT_SECRET: z.string(),
+  },
+
+  client: {},
+
+  experimental__runtimeEnv: {},
+
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+
+  emptyStringAsUndefined: true,
+});
