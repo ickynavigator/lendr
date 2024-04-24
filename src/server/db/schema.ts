b43a9d42a67e7d7a04c0e36bbcc795ef0042764e
Obi Fortune <gabrielobi.of@gmail.com>
2024-04-23 23:19:26 -0400

    update schemas

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from "crypto";
 import { relations, sql } from "drizzle-orm";
 import {
   index,
@@ -29,13 +30,15 @@ export const posts = sqliteTable(
 
 // #region NEXT-AUTH
 export const users = sqliteTable("user", {
-  id: text("id", { length: 255 }).notNull().primaryKey(),
-  name: text("name", { length: 255 }),
-  email: text("email", { length: 255 }).notNull(),
-  emailVerified: int("emailVerified", {
-    mode: "timestamp",
-  }).default(sql`CURRENT_TIMESTAMP`),
-  image: text("image", { length: 255 }),
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => randomUUID()),
+  name: text("name"),
+  email: text("email").notNull().unique(),
+  emailVerified: int("emailVerified", { mode: "timestamp" }).default(
+    sql`CURRENT_TIMESTAMP`,
+  ),
+  image: text("image"),
 });
 
 export const usersRelations = relations(users, ({ many }) => ({
@@ -45,21 +48,19 @@ export const usersRelations = relations(users, ({ many }) => ({
 export const accounts = sqliteTable(
   "account",
   {
-    userId: text("userId", { length: 255 })
+    userId: text("userId")
       .notNull()
-      .references(() => users.id),
-    type: text("type", { length: 255 })
-      .$type<AdapterAccount["type"]>()
-      .notNull(),
-    provider: text("provider", { length: 255 }).notNull(),
-    providerAccountId: text("providerAccountId", { length: 255 }).notNull(),
+      .references(() => users.id, { onDelete: "cascade" }),
+    type: text("type").$type<AdapterAccount["type"]>().notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
     refresh_token: text("refresh_token"),
     access_token: text("access_token"),
     expires_at: int("expires_at"),
-    token_type: text("token_type", { length: 255 }),
-    scope: text("scope", { length: 255 }),
+    token_type: text("token_type"),
+    scope: text("scope"),
     id_token: text("id_token"),
-    session_state: text("session_state", { length: 255 }),
+    session_state: text("session_state"),
   },
   (account) => ({
     compoundKey: primaryKey({
@@ -76,10 +77,13 @@ export const accountsRelations = relations(accounts, ({ one }) => ({
 export const sessions = sqliteTable(
   "session",
   {
-    sessionToken: text("sessionToken", { length: 255 }).notNull().primaryKey(),
-    userId: text("userId", { length: 255 })
+    id: text("id")
+      .primaryKey()
+      .$defaultFn(() => randomUUID()),
+    sessionToken: text("sessionToken").notNull().unique(),
+    userId: text("userId")
       .notNull()
-      .references(() => users.id),
+      .references(() => users.id, { onDelete: "cascade" }),
     expires: int("expires", { mode: "timestamp" }).notNull(),
   },
   (session) => ({
